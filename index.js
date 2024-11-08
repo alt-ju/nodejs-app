f0eea569a2362a64b2e22eb66b18115bdbc3be53
Justine Bouin <112966252+alt-ju@users.noreply.github.com>
2024-11-08 10:07:02 +0100

    feat: prepare deployment

--- index.js
+++ index.js
@@ -58,7 +58,10 @@ registerUsersRoutes(fastify)
 registerCategoryRoutes(fastify)
 
 try {
-    await fastify.listen({ port : 3000 })
+    await fastify.listen({ 
+        port: process.env.PORT || 3000,
+        host: process.env.HOST || 'localhost'
+    })
     await fastify.ready()
 } catch (err) {
     fastify.log.error(err)
